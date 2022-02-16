b1f3b78fe524aa1fc6113aff273d495d8a31b0cb
Lucas <wagashisakatsuki@gmail.com>
2022-02-16 14:59:07 +0100

    Ajout du component StylizedButton

--- src/screens/ScreenProfil.js
+++ src/screens/ScreenProfil.js
@@ -1,5 +1,6 @@
-import { Button, Text, View } from "react-native";
-import Dropdown from "../components/Dropdown.js"
+import { Text, View } from "react-native";
+import { StylizedButton } from "../components/StylizedButton/StylizedButton.js";
+import Dropdown from "../components/Dropdown.js";
 
 export default function ScreenProfil({route, navigation})
 {
@@ -9,7 +10,8 @@ export default function ScreenProfil({route, navigation})
 		<View style={{flex:1, backgroundColor:'#9FF'}}>
 			<Text>Choisissez votre langue : </Text>
 		<Dropdown />
-		<Button>Valider</Button>
+		<StylizedButton buttonStyle="btn--primary--primary" buttonSize="btn--medium" >Valider</StylizedButton>
+		<StylizedButton buttonStyle="btn--primary--outline" buttonSize="btn--medium" >Retour</StylizedButton>
 		</View>
 	)
 }
