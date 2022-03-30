4b063e186165f398bf15eb0e4ed6d33a7f4dd48f
Yanis <belkadi.yanis.@gmail.com>
2022-03-30 09:35:42 +0200

    design btn add avatar et avatar par default

--- src/components/ChoixImage.js
+++ src/components/ChoixImage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform, StyleSheet, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { Asset } from 'expo-asset';
 
+const avatarDefault = Asset.fromModule(require('../../assets/profileDefault.png')).uri;
 export default function ChoixImage() {
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState(avatarDefault);
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -22,30 +24,45 @@ export default function ChoixImage() {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.propsAvatar}>
 
 
       {image && <Image source={{ uri: image }} style={styles.img}/>}
 
-        <View style={styles.lol}>
-            <Text onPress={pickImage} style={{color: "#fff"}}>+</Text>
+        <View style={styles.btnAdd}>
+            <Text onPress={pickImage} style={styles.btnInput}  >+</Text>
         </View>
     </View>
 
   )}
 
 const styles = StyleSheet.create({
+  propsAvatar: { 
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
     img: {
         width: 120,
         height: 120,
         borderRadius: 10,
     },
-    lol: {
+    btnAdd: {
         position: "absolute",
-        backgroundColor: 'transparent',
-        color: "#fff",
-        right: 4,
         bottom: 2,
-        fontSize: 10,
+        right:2,
+        fontWeight: "bold",
+    },
+    btnInput: {
+      color: "white",
+      fontSize: 15,
+      backgroundColor: "#5593CF",
+      width: 20,
+      height: 20,
+      borderRadius: 20 / 2,
+      borderWidth: 1,
+      textAlign: "center",
     },
 });
